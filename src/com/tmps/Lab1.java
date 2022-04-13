1ba083733b70fa9f2edc645fcec705e347d19c75
munteanuion <munteanu.ion@isa.utm.md>
2022-04-13 09:29:50 +0300

    sustinut

--- src/com/tmps/Lab1.java
+++ src/com/tmps/Lab1.java
@@ -1,27 +1,46 @@
 package com.tmps;
 
+import com.sun.jdi.InternalException;
+
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Semaphore;
 
-public class Lab1 {
+public class Lab1 extends Thread {
+    Semaphore downloadImage;
+
     String query = "http://me.utm.md";
 
     HttpURLConnection connection = null;
 
     StringBuilder sb = new StringBuilder();
-    String string;
-
+    String response;
     String[] parts;
-
     String[] result1;
     String[] result2;
 
     ArrayList<String> imgArray = new ArrayList<String>();
+    ArrayList<String> imgArrayClean = new ArrayList<String>();
 
-    public void MainMain()
+    Set<String> imgs = new HashSet<>();
+
+    public Lab1(int countForSemaphore) {
+        this.downloadImage = new Semaphore(countForSemaphore);
+    }
+
+    public void ExtragereArrImg()
     {
         try{
             connection = (HttpURLConnection) new URL(query).openConnection();
@@ -53,8 +72,8 @@ public class Lab1 {
             }
         }
 
-        string = sb.toString();
-        parts = string.split("<img");
+        response = sb.toString();
+        parts = response.split("<img");
 
         for (int i=1; i < parts.length; i++)
         {
@@ -62,9 +81,13 @@ public class Lab1 {
             ExtragerePath("src='","'",i);
         }
 
-        System.out.println(imgArray);
-
         connection.disconnect();
+
+        imgArray.forEach((n) ->
+        {
+            result1 = n.split("/");
+            imgArrayClean.add(result1[result1.length-1]);
+        });
     }
 
     private void ExtragerePath(String cond1,String cond2, int i)
@@ -73,10 +96,45 @@ public class Lab1 {
         if(result1.length != 1)
         {
             result2 = result1[1].split(cond2,2);
-            if (result2[0].matches(".*.jpg.*") || result2[0].matches(".*.jpeg.*") || result2[0].matches(".*.png.*")){
+            if (result2[0].matches(".*.jpg.*") || result2[0].matches(".*.jpeg.*") || result2[0].matches(".*.png.*"))
+            {
                 imgArray.add(result2[0]);
             }
         }
+    }
+
+    @Override
+    public void run()
+    {
+        imgArray.forEach((n) ->
+        {
+            String url;
+            if (n.contains("http")){
+                url = "";
+            }else{
+                url = query+"/";
+            }
+            try
+            {
+                downloadImage.acquire();
+                System.out.println(url+n);
+                try(InputStream in = new URL(url+n).openStream()){
+                    Files.copy(in, Paths.get("C:\\Users\\ionio\\IdeaProjects\\lab1-PR\\src\\img\\"+imgArrayClean.get(imgArray.indexOf(n))), StandardCopyOption.REPLACE_EXISTING);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }  finally {
+                    downloadImage.release();
+                }
+
+            }catch (InternalException e){
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        });
     }
 }
 
--- src/com/tmps/Main.java
+++ src/com/tmps/Main.java
@@ -1,8 +1,18 @@
 package com.tmps;
 
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+
 public class Main {
     public static void main(String[] args) {
-        Lab1 lab1 = new Lab1();
-        lab1.MainMain();
+        Lab1 lab1 = new Lab1(2);
+        // Semaphore thread = new Semaphore(2);
+
+        //lab1.downloadImage = thread;
+
+        lab1.ExtragereArrImg();
+        System.out.println(lab1.imgArrayClean);
+        lab1.start();
     }
 }
